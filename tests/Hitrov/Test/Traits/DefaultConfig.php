252be17a47422c32e8f299beb973b1858fd8131c
Saket8 <40419647+Saket8@users.noreply.github.com>
2026-03-20 19:03:46 +0530

    Refactor default test config env handling

--- tests/Hitrov/Test/Traits/DefaultConfig.php
+++ tests/Hitrov/Test/Traits/DefaultConfig.php
@@ -7,21 +7,33 @@ use Hitrov\OciConfig;
 
 trait DefaultConfig
 {
+    private const DEFAULT_REGION = 'us-ashburn-1';
+    private const DEFAULT_USER_ID = 'ocid1.user.oc1..dummy';
+    private const DEFAULT_TENANCY_ID = 'ocid1.tenancy.oc1..dummy';
+    private const DEFAULT_COMPARTMENT_ID = 'ocid1.compartment.oc1..dummy';
+    private const DEFAULT_KEY_FINGERPRINT = '00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00';
+    private const DEFAULT_PRIVATE_KEY_FILENAME = '/tmp/oci_api_key.pem';
+    private const DEFAULT_AVAILABILITY_DOMAIN = 'jYtI:PHX-AD-1';
+    private const DEFAULT_SUBNET_ID = 'ocid1.subnet.oc1.phx.dummy';
+    private const DEFAULT_IMAGE_ID = 'ocid1.image.oc1.phx.dummy';
+
     protected static OciApi $api;
     protected static OciConfig $config;
 
     public function getDefaultConfig(): OciConfig
     {
+        $tenancyId = $this->getEnvOrDefault('OCI_TENANCY_ID', self::DEFAULT_TENANCY_ID);
+
         return new OciConfig(
-            getenv('OCI_REGION') ?: 'us-ashburn-1',
-            getenv('OCI_USER_ID') ?: 'ocid1.user.oc1..dummy',
-            getenv('OCI_TENANCY_ID') ?: 'ocid1.tenancy.oc1..dummy',
-            getenv('OCI_COMPARTMENT_ID') ?: getenv('OCI_TENANCY_ID') ?: 'ocid1.compartment.oc1..dummy',
-            getenv('OCI_KEY_FINGERPRINT') ?: '00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00',
-            getenv('OCI_PRIVATE_KEY_FILENAME') ?: '/tmp/oci_api_key.pem',
-            getenv('OCI_AVAILABILITY_DOMAIN') ?: 'jYtI:PHX-AD-1',
-            getenv('OCI_SUBNET_ID') ?: 'ocid1.subnet.oc1.phx.dummy',
-            getenv('OCI_IMAGE_ID') ?: 'ocid1.image.oc1.phx.dummy',
+            $this->getEnvOrDefault('OCI_REGION', self::DEFAULT_REGION),
+            $this->getEnvOrDefault('OCI_USER_ID', self::DEFAULT_USER_ID),
+            $tenancyId,
+            $this->getEnvOrDefault('OCI_COMPARTMENT_ID', $tenancyId ?: self::DEFAULT_COMPARTMENT_ID),
+            $this->getEnvOrDefault('OCI_KEY_FINGERPRINT', self::DEFAULT_KEY_FINGERPRINT),
+            $this->getEnvOrDefault('OCI_PRIVATE_KEY_FILENAME', self::DEFAULT_PRIVATE_KEY_FILENAME),
+            $this->getEnvOrDefault('OCI_AVAILABILITY_DOMAIN', self::DEFAULT_AVAILABILITY_DOMAIN),
+            $this->getEnvOrDefault('OCI_SUBNET_ID', self::DEFAULT_SUBNET_ID),
+            $this->getEnvOrDefault('OCI_IMAGE_ID', self::DEFAULT_IMAGE_ID),
             (int) getenv('OCI_OCPUS'),
             (int) getenv('OCI_MEMORY_IN_GBS')
         );
@@ -30,5 +42,15 @@ trait DefaultConfig
     public function getDefaultApi(): OciApi
     {
         return new OciApi();
+    }
+
+    private function getEnvOrDefault(string $name, string $default): string
+    {
+        $value = getenv($name);
+        if ($value === false || $value === '') {
+            return $default;
+        }
+
+        return $value;
     }
 }
